17a38bd1fe9e9d2738615fb6092a7274fd599ea2
johnnque <johnn_patrick_que@dlsu.edu.ph>
2026-04-12 21:38:06 +0800

    ISR for sensors

--- main.c
+++ main.c
@@ -7,14 +7,26 @@
 
 
 #include <xc.h>
-
 #define _XTAL_FREQ 20000000 
+
+#include <RBInt.h>
+#include <Sensor.h>
+
+uint8_t sensorReading = 0;
+
+void __interrupt ISR(void){
+    if(INTCON & 0x01){
+        sensorReading = Sensor_read();
+        INTCON &= ~0x01;
+    }
+    return;
+}
 
 void main(void) {
 
     while(1){
 
-        
+
     }
     return;
 }
