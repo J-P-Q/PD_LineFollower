829b97d2ce3996101d227afbfca86f4c127e23a3
johnnque <johnn_patrick_que@dlsu.edu.ph>
2026-04-15 19:58:45 +0800

    Fixed timer computations

--- TMR0.c
+++ TMR0.c
@@ -1,17 +1,23 @@
 #include <xc.h>
 
+#define preloadCounter 195
+#define timerPrescaler 0x07
+
 void TMR0_init(void){   //100ms overflow
     
     // Tic/Sec = 20MHz / (4 * 256) = 19531.25
-    // Tic/100ms = 19531.25 / 100 = 195.3125
-    // 256 - 195.3125 = 60.6875 ~ 61
-    TMR0 = 61;
-    OPTION_REG |= 4; // prescaler = 32
+    // 1/19531.25 = 51.2us
+    // 51.2us * 256 = 13.1072ms
+
+    // 10ms / 51.2us = 195.3125 
+
+    TMR0 = preloadCounter;
+    OPTION_REG |= timerPrescaler; // prescaler = 256
     INTCON |= (1 << 5);
     return;
 }
 
 void TMR0_reset(void){
-    TMR0 = 61;
+    TMR0 = preloadCounter;
     return;
 }
