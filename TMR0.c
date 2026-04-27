2545f1eb3dc72e65668d1b21f908ef0682042098
johnnque <johnn_patrick_que@dlsu.edu.ph>
2026-04-27 21:59:30 +0800

    Increase Sensor Reading Frequency
    from 10ms to 5ms delay although didnt rename the main.c my10ms variable to 5ms

--- TMR0.c
+++ TMR0.c
@@ -1,6 +1,6 @@
 #include <xc.h>
 
-#define preloadCounter 61
+#define preloadCounter 158
 #define timerPrescaler 0x07
 
 void TMR0_init(void){   //100ms overflow
@@ -9,8 +9,13 @@ void TMR0_init(void){   //100ms overflow
     // 1/19531.25 = 51.2us
     // 51.2us * 256 = 13.1072ms
 
+    // Old calculations     (265 instead of 256, but it was close enough to 10ms)
     // 10ms / 51.2us = 195.3125 
-    // 265 - 195.3125 = 61
+    // 256 - 195.3125 = 60
+
+    // New calculations     (try 5ms)
+    // 5ms / 51.2us = 97.65625
+    // 256 - 97.65625 = 158.34375 (rounded to 158)
 
     TMR0 = preloadCounter;
     OPTION_REG = (OPTION_REG & 0xF8) | timerPrescaler; // prescaler = 256
